b87225ab88ecdc995d3c6c31441da84fbb351aa7
Can Akdag <canakdag06@gmail.com>
2026-02-12 18:10:30 +0300

    created namespaces

---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+
+        [SerializeField] private NavMeshAgent agent;
+        [SerializeField] private Animator animator;
+
+        private const string ForwardSpeed = "forwardSpeed";
+
+        void Update()
+        {
+            UpdateAnimator();
+        }
+
+        public void MoveTo(Vector3 position)
+        {
+            agent.destination = position;
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = agent.velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            animator.SetFloat(ForwardSpeed, speed);
+        }
+    }
+}
