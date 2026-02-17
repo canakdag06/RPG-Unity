2ebe497975964c2b59e6d8246e5c459b2b80fb1a
Can Akdag <canakdag06@gmail.com>
2026-02-17 19:41:46 +0300

    Update PlayerController.cs

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -39,7 +39,7 @@ namespace RPG.Control
             {
                 CombatTarget target = hit.transform.GetComponent<CombatTarget>();
 
-                if(!target.GetComponent<Fighter>().CanAttack(target))
+                if(!GetComponent<Fighter>().CanAttack(target))
                 {
                     continue;
                 }
