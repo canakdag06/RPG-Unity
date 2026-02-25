99810933826ca70f4dbe8ab2291664e86ea90cf2
Can Akdag <canakdag06@gmail.com>
2026-02-25 11:12:47 +0300

    added SpawnPoints

--- Assets/Scripts/Scene Management/Portal.cs
+++ Assets/Scripts/Scene Management/Portal.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 
@@ -8,6 +9,7 @@ namespace RPG.SceneManagement
     public class Portal : MonoBehaviour
     {
         [SerializeField] private int sceneIndexToLoad = 0;
+        [SerializeField] private Transform spawnPoint;
 
         private void OnTriggerEnter(Collider other)
         {
@@ -21,8 +23,30 @@ namespace RPG.SceneManagement
         {
             DontDestroyOnLoad(gameObject);
             yield return SceneManager.LoadSceneAsync(sceneIndexToLoad);
+
+            Portal otherPortal = FindOtherPortal();
+            UpdatePlayer(otherPortal);
             print("Scene loaded");
             Destroy(gameObject);
+        }
+
+
+        private Portal FindOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsByType<Portal>(FindObjectsSortMode.None))
+            {
+                if (portal == this) continue;
+
+                return portal;
+            }
+            return null;
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+            player.transform.rotation = otherPortal.spawnPoint.rotation;
         }
     }
 }
