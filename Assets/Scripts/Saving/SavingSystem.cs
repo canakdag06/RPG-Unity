25ec848572a31a7e5bbcd5be4098119936f3351d
Can Akdag <canakdag06@gmail.com>
2026-04-17 22:02:02 +0300

    Update SavingSystem.cs

--- Assets/Scripts/Saving/SavingSystem.cs
+++ Assets/Scripts/Saving/SavingSystem.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using UnityEngine;
 
 namespace RPG.Saving
@@ -6,12 +8,59 @@ namespace RPG.Saving
     {
         public void Save(string saveFile)
         {
-            Debug.Log("Saving to " + saveFile);
+            string path = GetPathFromSaveFile(saveFile);
+            Debug.Log("Saving to " + path);
+
+            using (FileStream stream = File.Open(path, FileMode.Create))
+            {
+                Transform playerTransform = GetPlayerTransform();
+                byte[] buffer = SerializeVector(playerTransform.position);
+                stream.Write(buffer, 0, buffer.Length);
+            }
         }
 
         public void Load(string saveFile)
         {
-            Debug.Log("Loading from " + saveFile);
+            string path = GetPathFromSaveFile(saveFile);
+            Debug.Log("Loading from " + path);
+
+            using (FileStream stream = File.Open(path, FileMode.Open))
+            {
+                byte[] buffer = new byte[stream.Length];
+                stream.Read(buffer, 0, buffer.Length);
+
+                Transform playerTransform = GetPlayerTransform();
+                playerTransform.position = DeserializeVector(buffer);
+            }
+        }
+
+        private string GetPathFromSaveFile(string saveFile)
+        {
+            return $"{Application.persistentDataPath}/{saveFile}.sav";
+            //return Path.Combine(Application.persistentDataPath, saveFile + ".sav");
+        }
+
+        private Transform GetPlayerTransform()
+        {
+            return GameObject.FindWithTag("Player").transform;
+        }
+
+        private byte[] SerializeVector(Vector3 vector)
+        {
+            byte[] vectorBytes = new byte[12]; // because Vector3 has 3 floats, and each float is 4 bytes
+            BitConverter.GetBytes(vector.x).CopyTo(vectorBytes, 0);
+            BitConverter.GetBytes(vector.y).CopyTo(vectorBytes, 4);
+            BitConverter.GetBytes(vector.z).CopyTo(vectorBytes, 8);
+            return vectorBytes;
+        }
+
+        private Vector3 DeserializeVector(byte[] buffer)
+        {
+            Vector3 result = new Vector3();
+            result.x = BitConverter.ToSingle(buffer, 0);
+            result.y = BitConverter.ToSingle(buffer, 4);
+            result.z = BitConverter.ToSingle(buffer, 8);
+            return result;
         }
     }
 }
