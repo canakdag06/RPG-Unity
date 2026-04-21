a653b78fc474da2561747b585a1bd476f3af0935
Can Akdag <canakdag06@gmail.com>
2026-04-21 10:14:52 +0300

    Update SaveableEntity.cs

--- Assets/Scripts/Saving/SaveableEntity.cs
+++ Assets/Scripts/Saving/SaveableEntity.cs
@@ -1,12 +1,34 @@
+using UnityEditor;
 using UnityEngine;
 
 namespace RPG.Saving
 {
+    [ExecuteAlways]
     public class SaveableEntity : MonoBehaviour
     {
+        [SerializeField] string ID = "";
+
+#if UNITY_EDITOR
+        private void Update()
+        {
+            if (Application.IsPlaying(gameObject)) return;
+            if (string.IsNullOrEmpty(gameObject.scene.path)) return;
+
+            SerializedObject sObject = new SerializedObject(this);
+            SerializedProperty property = sObject.FindProperty("ID");
+
+            if (property.stringValue == "")
+            {
+                property.stringValue = System.Guid.NewGuid().ToString();
+                sObject.ApplyModifiedProperties();
+            }
+
+            print("Editing");
+        }
+#endif
         public string GetID()
         {
-            return "";
+            return ID;
         }
 
         public object CaptureState()
