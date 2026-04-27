341a232ed9f92b1b4571d8e38b2a50e0a6e8d7a6
Can Akdag <canakdag06@gmail.com>
2026-04-27 18:05:07 +0300

    created Pickups

---
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace RPG.Combat
+{
+
+    public class WeaponPickup : MonoBehaviour
+    {
+        [SerializeField] Weapon weapon = null;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                other.GetComponent<Fighter>().EquipWeapon(weapon);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
