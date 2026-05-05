4690bf4ec6e9747f8681fc452a6af9151382e56e
Can Akdag <canakdag06@gmail.com>
2026-05-06 00:23:03 +0300

    respawning Weapon Pickups

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace RPG.Combat
@@ -6,13 +7,32 @@ namespace RPG.Combat
     public class WeaponPickup : MonoBehaviour
     {
         [SerializeField] Weapon weapon = null;
+        private float respawnDelay = 2f;
 
         private void OnTriggerEnter(Collider other)
         {
             if (other.CompareTag("Player"))
             {
                 other.GetComponent<Fighter>().EquipWeapon(weapon);
-                Destroy(gameObject);
+                StartCoroutine(HideForSeconds(respawnDelay));
+                //Destroy(gameObject);
+            }
+        }
+
+        private IEnumerator HideForSeconds(float delay)
+        {
+            SetVisibility(false);
+            yield return new WaitForSeconds(delay);
+            SetVisibility(true);
+        }
+
+        private void SetVisibility(bool isVisible)
+        {
+            GetComponent<Collider>().enabled = isVisible;
+
+            foreach (Transform child in transform)
+            {
+                child.gameObject.SetActive(isVisible);
             }
         }
     }
