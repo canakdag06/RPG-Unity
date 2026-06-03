58d76d8265515daee08594c6c981e35b60d196fb
Can Akdag <canakdag06@gmail.com>
2026-06-03 16:10:35 +0300

    updated Progression SO

--- Assets/Scripts/Core/Progression.cs
+++ Assets/Scripts/Core/Progression.cs
@@ -1,7 +1,27 @@
+using RPG.Stats;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Progression", menuName = "Stats/New Progression", order = 0)]
-public class Progression : ScriptableObject
+namespace RPG.Stats
 {
+    [CreateAssetMenu(fileName = "Progression", menuName = "Stats/New Progression", order = 0)]
+    public class Progression : ScriptableObject
+    {
+        [SerializeField] ProgressionCharacterClass[] characterClasses = null;
 
+        [System.Serializable]
+        public class ProgressionCharacterClass
+        {
+            [SerializeField] CharacterClass characterClass;
+            [SerializeField] ProgressionLevel[] levels;
+        }
+
+        [System.Serializable]
+        public class ProgressionLevel
+        {
+            [SerializeField] int level;
+            [SerializeField] float health;
+            [SerializeField] float damage;
+        }
+
+    }
 }
