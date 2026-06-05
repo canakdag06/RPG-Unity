57fd1bb679089097eee721f0839b2e2d617a4828
Can Akdag <canakdag06@gmail.com>
2026-06-05 17:53:21 +0300

    Experience.cs implements ISaveable now

--- Assets/Scripts/Attributes/Experience.cs
+++ Assets/Scripts/Attributes/Experience.cs
@@ -1,15 +1,26 @@
+using RPG.Saving;
 using UnityEngine;
 
 
 namespace RPG.Attributes
 {
-    public class Experience : MonoBehaviour
+    public class Experience : MonoBehaviour, ISaveable
     {
         [SerializeField] float experiencePoints = 0f;
 
         public void GainEXP(float exp)
         {
             experiencePoints += exp;
+        }
+
+        public object CaptureState()
+        {
+            return experiencePoints;
+        }
+
+        public void RestoreState(object state)
+        {
+            experiencePoints = (float)state;
         }
     }
 }
